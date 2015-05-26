187d45d084e0d1f9e5d27384eb6c875e5828aee6
Rob Kendrick <rjek@rjek.com>
2015-05-26 19:17:04 +0100

    Change URL to repository

--- rbldnsdise.sh
+++ rbldnsdise.sh
@@ -2,5 +2,5 @@
 # usage: rbldnsdise.sh < hepworth.txt > hepworth.db
 #        rbldnsd excommunicado.co.uk:dnset:hepworth.db
 
-echo :127.0.0.2:$ listed in excommunicado.co.uk, see https://github.com/antibodyMX/communicado
+echo :127.0.0.2:$ listed in excommunicado.co.uk, see https://github.com/excommunicado/communicado
 exec sed -e '/^mailer\./d' -e 's/^/./'
